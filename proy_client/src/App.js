68f874d8683e7d65fd560cf659dfb4ea965a9ee6
GeraldineRomer <geraldine.rome0104@gmail.com>
2023-05-15 10:19:21 -0500

    register admin & change password

--- proy_client/src/App.js
+++ proy_client/src/App.js
@@ -3,8 +3,10 @@ import React from 'react'
 import './App.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AllRoutesProject from './config/routes'
-import  SignIn  from './pages/SIgIn/SignIn'
-import { LayoutGenreal } from './layouts/GeneralLayout/LayoutGerenal'
+import  SignIn  from './pages/SignIn/SignIn'
+import { LayoutGenreal } from './layouts/GeneralLayout/LayoutGenreal'
+import AdminRegister from './pages/AdminRegister/AdminRegister'
+import ChangePassword from './pages/ChangePassword/ChangePassword'
 
 const App = () => {
   return (
@@ -12,7 +14,8 @@ const App = () => {
       <Routes>
         <Route path='/' element={<SignIn/>}></Route>
         <Route path='dashboard' element={<LayoutGenreal/>}></Route>
-
+        <Route path='adminregister' element={<AdminRegister/>}></Route>
+        <Route path='changepassword' element={<ChangePassword/>}></Route>
         {AllRoutesProject.map((route, index) => (
           <Route
             key={index}
--- proy_client/src/config/routes.js
+++ proy_client/src/config/routes.js
@@ -1,18 +1,23 @@
 /* components > Layouts > Pages > routes */
-import { LayoutGenreal } from "../layouts/GeneralLayout/LayoutGerenal";
+import { LayoutGenreal } from "../layouts/GeneralLayout/LayoutGenreal";
 import { Admin } from "../pages/admin/Admin";
-import  SignIn  from "../pages/SIgIn/SignIn";
+import  SignIn  from "../pages/SignIn/SignIn";
+import AdminRegister from "../pages/AdminRegister/AdminRegister";
+import ChangePassword from "../pages/ChangePassword/ChangePassword";
 import { Contact } from "../pages/Contact";
 import { Home } from "../pages/Home";
 import { NotFound } from "../pages/NotFound/NotFound";
+
 const AdminRoutes = [
     {path: '/admin', component: Admin, layout: LayoutGenreal},
-    {path: '/admin/sign-in', component: SignIn,layout: LayoutGenreal}
+    {path: '/admin/sign-in', component: SignIn,layout: LayoutGenreal},
+    {path: '/admin/admin-register', component: AdminRegister,layout: LayoutGenreal}
 ];
 const GeneralRoutes = [
     {path: '/',component: Home,layout: LayoutGenreal},
     {path: '/contact',component: Contact,layout: LayoutGenreal},
-    {path: '*',component: NotFound,layout: LayoutGenreal}
+    {path: '*',component: NotFound,layout: LayoutGenreal},
+    {path: 'change-password',component: ChangePassword,layout: LayoutGenreal}
 ];
 
 const allRoutesProject = [...AdminRoutes,...GeneralRoutes];
