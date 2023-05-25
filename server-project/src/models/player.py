a0884000cee1bc6c0218c15a3a951463b3b11575
marcelagiraldo <leidymarcelaalzategiraldo@gmail.com>
2023-05-25 02:03:14 -0500

    update

--- server-project/src/models/player.py
+++ server-project/src/models/player.py
@@ -1,21 +1,16 @@
-from werkzeug.security import generate_password_hash,check_password_hash
-
 class Player:
-    def __init__(self,document,type_document,name,lastname,email,password):
+    def __init__(self,document,type_document,name,lastname,email):
         self.document = document
         self.type_document = type_document
         self.name = name
         self.last_name = lastname
         self.email = email
-        self.password = password
 
     def toDBCollection(self):
-        hashed = generate_password_hash(self.password)
         return{
             "document":self.document,
             "type_document":self.type_document,
             "name":self.name,
             "lastname":self.last_name,
-            "email":self.email,
-            "password":hashed
+            "email":self.email
         }
