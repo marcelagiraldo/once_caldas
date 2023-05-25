dac5eb67039f6223dc228054d0b81a364ca62cba
marcelagiraldo <leidymarcelaalzategiraldo@gmail.com>
2023-05-25 01:47:12 -0500

    CRUD admin and students

--- server-project/constants.js
+++
@@ -1,9 +0,0 @@
-const DB_HOST = "cluster0.ykmy0lr.mongodb.net";
-const DB_USER = "project_server";
-const DB_PASSWORD = "project_server";
-const DB_NAME = "project_server_DB";
-
-const API_VERSION = "v1";
-const IP_SERVER = "localhost";
-const JWT_SECRET_KEY = "acceso-sistema-software2-2023"
-module.exports= {DB_HOST,DB_USER,DB_PASSWORD,DB_NAME,API_VERSION,IP_SERVER,JWT_SECRET_KEY};
