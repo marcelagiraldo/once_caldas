62d54ae8edcc8b6a09e6535e065953b90216f482
yansaid21 <yansaid21@gmail.com>
2023-05-28 14:04:06 -0500

    save button

--- proy_client/src/components/UsersComponents/UserButton/UserButton.js
+++ proy_client/src/components/UsersComponents/UserButton/UserButton.js
@@ -4,7 +4,7 @@ import "./UserButton.scss"
 export const UserButton = (props) => {
     const {PersonName} = props;
     return(
-  <Space wrap>
+  <Space wrap className='UserSpace'>
     <Button type="primary" className='UserButton'>{PersonName}</Button>
   </Space>)
 };
--- proy_client/src/pages/StudentsList/StudentsList.js
+++ proy_client/src/pages/StudentsList/StudentsList.js
@@ -3,13 +3,19 @@ import { UserButton } from "../../components/UsersComponents/UserButton/UserButt
 import { DeactivateButton } from "../../components/UsersComponents/DeactivateButton/DeactivateButton";
 import { DashboardTop } from "../../components/TopComponents/DashboardTop/DashboardTop"
 import "./StudentsList.scss"
-import { Col } from "antd";
+import { Button, Col, Input } from "antd";
+import { FileSearchOutlined, SearchOutlined } from "@ant-design/icons";
 
 export const StudentsList = () => {
   return (
     <div>
         <DashboardTop addtitle="ESTUDIANTES" userName="Lina Maria Montereal Mesa" />
-        <div className="UsersList">
+        <div className="filter">
+          <Input classname="filterInput" prefix={<SearchOutlined/>}/>
+          <Button className="filterButton"><FileSearchOutlined /></Button>
+        </div>
+    <div className="StudentsList">
+    
         <Col>
         <UserButton PersonName="juan carlo otalvaro" />
         </Col>
