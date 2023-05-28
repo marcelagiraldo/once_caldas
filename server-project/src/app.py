4e028d539ae74f13eae75666eb54403e09b58a69
GeraldineRomer <geraldine.rome0104@gmail.com>
2023-05-28 00:15:54 -0500

    crud begin players

--- server-project/src/app.py
+++ server-project/src/app.py
@@ -1,12 +1,13 @@
 from flask import Flask,request
 from endpoints.students import students
 from endpoints.admins import admins
+from endpoints.players import players
 
 app = Flask(__name__)
 
 app.register_blueprint(students)
 app.register_blueprint(admins)
-
+app.register_blueprint(players)
 
 if __name__ == "__main__":
     app.run(debug=True)
