aacef84309ed970c9bd8f4ca896be0449a798850
marcelagiraldo <leidymarcelaalzategiraldo@gmail.com>
2023-05-28 00:05:53 -0500

    connection front to back

--- proy_client/src/App.js
+++ proy_client/src/App.js
@@ -1,18 +1,33 @@
 /* rafce */
-import React from 'react'
+import React,{ useEffect, useState } from 'react'
 import './App.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AllRoutesProject from './config/routes'
 import  SignIn  from './pages/SIgIn/SignIn'
 import { LayoutGenreal } from './layouts/GeneralLayout/LayoutGerenal'
+import axios from 'axios';
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/v1/admins');
+      setData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  console.log(data)
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<SignIn/>}></Route>
         <Route path='dashboard' element={<LayoutGenreal/>}></Route>
-
         {AllRoutesProject.map((route, index) => (
           <Route
             key={index}
@@ -27,7 +42,10 @@ const App = () => {
       </Routes>
     </BrowserRouter>
   );
+
 };
 
 export default App
+
+
 
